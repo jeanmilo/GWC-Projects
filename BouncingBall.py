3ae19c6b57178f8a65147a1fbb530ee144bc2e28
Claudia Yamamoto <claudia.parlee@gmail.com>
2018-01-10 14:36:53 -0500

    Create BouncingBall.py

---
+++ BouncingBall.py
@@ -0,0 +1,55 @@
+"""
+ Pygame base template for opening a window
+
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+
+import pygame
+import random
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (127, 127, 127)
+
+
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Bouncing Ball Game")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+    
+    # --- Update
+    pygame.display.flip()
+
+    # --- 60 fps
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
+# Needed when using IDLE
+exit() 
